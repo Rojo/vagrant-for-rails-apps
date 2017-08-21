ccc2d32cfeab509e798d5bd0ae4ef560c8cfa981
David O' Rojo <david.orojo@gmail.com>
2017-08-21 17:29:23 -0500

    Set a correct .zshrc configuration

--- provision/zsh_setup.sh
+++ provision/zsh_setup.sh
@@ -12,9 +12,19 @@ if ! dpkg -s zsh; then
   git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
   cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
 
-  # Change the oh my zsh default theme.
-  sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="fino"/g' ~/.zshrc
-  sed -i 's/plugins=(git)"/plugins=(git dirhistory common-aliases command-not-found)/g' ~/.zshrc
+  # Change the Oh-My-Zsh! default configuration.
+  sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="blinks"/g' ~/.zshrc
+  sed -i 's/# CASE_SENSITIVE="true"/CASE_SENSITIVE="true"/g' ~/.zshrc
+  sed -i 's/# COMPLETION_WAITING_DOTS="true"/COMPLETION_WAITING_DOTS="true"/g' ~/.zshrc
+  sed -i 's/# DISABLE_UNTRACKED_FILES_DIRTY="true"/DISABLE_UNTRACKED_FILES_DIRTY="true"/g' ~/.zshrc
+  sed -i 's/# HIST_STAMPS="mm/dd/yyyy"/HIST_STAMPS="yyyy-mm-dd"/g' ~/.zshrc
+  sed -i 's/plugins=(git)/plugins=(git dirhistory common-aliases command-not-found ruby)/g' ~/.zshrc
+
+  # Include .profile settings
+  echo ' '  >> ~/.zshrc
+  echo '# Include .profile settings'  >> ~/.zshrc
+  echo 'source ~/.profile'  >> ~/.zshrc
+  echo ' '  >> ~/.zshrc
 
   # Change shell to Zsh for the ubuntu user
   sudo chsh -s /bin/zsh ubuntu
