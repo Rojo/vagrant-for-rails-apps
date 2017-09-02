a5ebe8f27317cb2e54c792566d8f2584fafec96a
David O' Rojo <david.orojo@gmail.com>
2017-09-02 14:01:01 -0500

    Include Yarn package manager

--- provision/box_setup.zsh
+++ provision/box_setup.zsh
@@ -100,3 +100,14 @@ if ! heroku --version; then
 else
   echo 'OK'
 fi
+
+# Install Yarn package manager ################################################
+
+echo "***************************************************"
+echo "Checking Yarn installation..."
+echo "***************************************************"
+if ! dpkg -s yarn; then
+ curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+ echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+ sudo apt-get update && sudo apt-get install yarn
+fi
